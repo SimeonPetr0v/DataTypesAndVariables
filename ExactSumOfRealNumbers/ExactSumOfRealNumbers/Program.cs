28134d5f2ce6b82c7f602115e8662576691c3912
SimeonPetr0v <simeonp925@gmail.com>
2023-10-03 12:03:03 +0300

    Exercises

---
+++ ExactSumOfRealNumbers/ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace ExactSumOfRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            decimal n = decimal.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                decimal num = decimal.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
